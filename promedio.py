32be1075c9e399d708c0b9f775161772d5a2d996
Jhon-GG <111620215+Jhon-GG@users.noreply.github.com>
2024-01-19 13:03:18 -0500

    Ejercicio promedio

---
+++ promedio.py
@@ -0,0 +1,10 @@
+# Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
+
+var1 = float(input("Ingrese la primer nota: \n"))
+var2 = float(input("Ingrese la segunda nota: \n"))
+var3 = float(input("Ingrese la tercer nota: \n"))
+var4 = float(input("Ingrese la cuarta nota: \n"))
+
+promedio = (var1 + var2 + var3 + var4) / 4
+
+print(f"El promedio de sus notas es: {promedio}")
